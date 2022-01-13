1d16c6a3ac12e8d2bcdcba1f21bb999bbbb11bfc
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2022-01-13 17:30:01 -0300

    passing values as props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const expenseDate = new Date(2021,2,28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.57;
+
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
