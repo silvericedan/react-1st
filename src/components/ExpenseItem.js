3ee2fdafc906280b397c77a675a1cd78f8b3d804
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2022-01-14 16:01:32 -0300

    separated components into ExpenseDate and Expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021,2,28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.57;
-
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
