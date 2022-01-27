e8e295ea66152214f554ac809b3512995848148c
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2022-01-27 17:23:53 -0300

    Using useState hook to set values

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,23 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
 
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+
   const clickHandler = () => {
-    console.log('Clicked!!!');
+    setTitle('Updated!');
+    console.log(title);
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
