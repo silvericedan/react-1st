ac62ce6619d613cd3176863b304631e89565c68b
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2022-09-18 16:07:52 -0300

    child to parent communication from NewExpense to App.js

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,14 +3,15 @@ import React from "react"; //this is not needed but just as a remainder.
 import ExpenseForm from './ExpenseForm'
 import '../NewExpense/NewExpense.css'
 
-const NewExpense = () => {
+const NewExpense = (props) => {
 
     const saveExpenseDataHandler= (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
-        console.log(expenseData);
+        //console.log(expenseData);
+        props.onAddExpense(expenseData);
     }
 
     return <div className="new-expense">
